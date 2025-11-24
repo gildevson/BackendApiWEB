b35daf626d1c39239fb9db21fca671a2b545a224
gilsonfonseca92@gmail.com <gilson.silva@finanblue.com.vc>
2025-11-24 13:02:08 -0300

    Adicionando tabelas no banco de dados

---
+++ BackendApiWEB/DataBase/UsuarioPermissao.sql
@@ -0,0 +1,34 @@
+﻿USE [SistemaPDV];
+GO
+
+CREATE TABLE [dbo].[UsuarioPermissao](
+    [Id] INT IDENTITY(1,1) NOT NULL,
+    [UsuarioId] UNIQUEIDENTIFIER NOT NULL,
+    [PermissaoId] INT NOT NULL,
+    CONSTRAINT [PK_UsuarioPermissao] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [UK_Usuario_Permissao] UNIQUE NONCLUSTERED (
+        [UsuarioId] ASC,
+        [PermissaoId] ASC
+    )
+);
+GO
+
+ALTER TABLE [dbo].[UsuarioPermissao]  WITH CHECK 
+ADD CONSTRAINT [FK_UsuarioPermissao_Permissao] 
+FOREIGN KEY([PermissaoId])
+REFERENCES [dbo].[Permissoes] ([Id])
+ON DELETE CASCADE;
+GO
+
+ALTER TABLE [dbo].[UsuarioPermissao] CHECK CONSTRAINT [FK_UsuarioPermissao_Permissao];
+GO
+
+ALTER TABLE [dbo].[UsuarioPermissao]  WITH CHECK 
+ADD CONSTRAINT [FK_UsuarioPermissao_Usuario] 
+FOREIGN KEY([UsuarioId])
+REFERENCES [dbo].[Usuarios] ([Id])
+ON DELETE CASCADE;
+GO
+
+ALTER TABLE [dbo].[UsuarioPermissao] CHECK CONSTRAINT [FK_UsuarioPermissao_Usuario];
+GO
