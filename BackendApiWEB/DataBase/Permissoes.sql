949216ece49d6aea73627b2faec16e24fb9fe170
gilsonfonseca92@gmail.com <gilson.silva@finanblue.com.vc>
2025-11-24 13:09:32 -0300

    Adicionando mais dados

--- BackendApiWEB/DataBase/Permissoes.sql
+++ BackendApiWEB/DataBase/Permissoes.sql
@@ -3,3 +3,9 @@
     Nome NVARCHAR(100) NOT NULL,
     Descricao NVARCHAR(200)
 );
+
+INSERT INTO Permissoes (Nome, Descricao)
+VALUES ('ADMIN', 'Administrador do sistema');
+
+INSERT INTO Permissoes (Nome, Descricao)
+VALUES ('USUARIO', 'Acesso básico ao sistema');
