754d6403eeeb1cdcd6c65d967d42054772f29a5c
gildevson <gildevson@gmail.com>
2026-01-16 20:12:48 -0300

    Adicionando mais uam classe

---
+++ BackendApiWEB/DTOs/CadastroClienteRepresentanteDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace BackendApiWEB.DTOs {
+    public class CadastroClienteRepresentanteDTO {
+        public PessoaCadastroDTO cliente { get; set; } = new() { TipoPessoa = "CLIENTE"};
+        public PessoaCadastroDTO representante { get; set; } = new() { TipoPessoa = "REPRESENTANTE" }; 
+    }
+}
---
+++ BackendApiWEB/Models/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿namespace BackendApiWEB.Models {
+    public class Pessoa {
+        public int id { get; set; }
+        public string TipoPessoa { get; set; } = ""; // CLIENTE / REPRESENTANTE
+        public string RazaoSocial { get; set; }
+        public string ? Fantasia { get; set; }
+        public string ? cep { get; set; }
+        public string ? Logradouro { get; set; }
+        public string ? Numero { get; set; }
+        public string ? Complemento { get; set; }
+        public string ? Bairro { get; set; }
+        public string ? Cidade { get; set; }
+        public string ? Estado { get; set; }
+        public string ? NomeContato { get; set; }
+        public string ? Email { get; set; }
+        public string? Telefone1 { get; set; }
+
+    }
+}
