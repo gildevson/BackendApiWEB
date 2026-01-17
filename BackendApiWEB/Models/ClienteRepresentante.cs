3cc496c346fe552e194a31e430761be442573d28
gildevson <gildevson@gmail.com>
2026-01-16 22:25:17 -0300

    adicionando mais commit

---
+++ BackendApiWEB/Models/ClienteRepresentante.cs
@@ -0,0 +1,6 @@
+﻿namespace SeuProjeto.Models;
+
+public class ClienteRepresentante {
+    public int ClienteId { get; set; }
+    public int RepresentanteId { get; set; }
+}
--- BackendApiWEB/Models/Pessoa.cs
+++ BackendApiWEB/Models/Pessoa.cs
@@ -1,19 +1,22 @@
 ﻿namespace BackendApiWEB.Models {
     public class Pessoa {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string TipoPessoa { get; set; } = ""; // CLIENTE / REPRESENTANTE
-        public string RazaoSocial { get; set; }
-        public string ? Fantasia { get; set; }
-        public string ? cep { get; set; }
-        public string ? Logradouro { get; set; }
-        public string ? Numero { get; set; }
-        public string ? Complemento { get; set; }
-        public string ? Bairro { get; set; }
-        public string ? Cidade { get; set; }
-        public string ? Estado { get; set; }
-        public string ? NomeContato { get; set; }
-        public string ? Email { get; set; }
-        public string? Telefone1 { get; set; }
 
+        public string CpfCnpj { get; set; } = "";
+        public string RazaoSocial { get; set; } = "";
+        public string? Fantasia { get; set; }
+
+        public string? Cep { get; set; }
+        public string? Logradouro { get; set; }
+        public string? Numero { get; set; }
+        public string? Complemento { get; set; }
+        public string? Bairro { get; set; }
+        public string? Cidade { get; set; }
+        public string? Estado { get; set; }
+
+        public string? NomeContato { get; set; }
+        public string? Email { get; set; }
+        public string? Telefone1 { get; set; }
     }
 }
