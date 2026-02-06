6bab6ae592bc4fad8809fff60909292b5ca3fb23
gildevson <gildevson@gmail.com>
2026-02-06 20:21:50 -0300

    Adicioanndo mais dados de mundanças

--- BackendApiWEB/Models/ClienteRepresentante.cs
+++ BackendApiWEB/Models/ClienteRepresentante.cs
@@ -1,6 +1,6 @@
 ﻿namespace SeuProjeto.Models;
 
-public class ClienteFornecedor {
+public class ClienteRepresentante {
     public int ClienteId { get; set; }
     public int FornecedorId { get; set; }
 }
