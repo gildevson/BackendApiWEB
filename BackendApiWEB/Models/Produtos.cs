ecacc45a15c31b6c3149d1727a2a8e2b7c5dbbd4
gildevson <gildevson@gmail.com>
2026-02-06 23:40:18 -0300

    Adicioanndo mais dados#

--- BackendApiWEB/Models/Produtos.cs
+++ BackendApiWEB/Models/Produtos.cs
@@ -1,4 +1,14 @@
 ﻿namespace BackendApiWEB.Models {
     public class Produtos {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public string ? Descricao { get; set; } // ? para dizer que é opcional pode ser Nula
+        public decimal Preco { get; set; }
+
+
+
+
+
+
     }
 }
