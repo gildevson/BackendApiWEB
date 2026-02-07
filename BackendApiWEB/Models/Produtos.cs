c6a9c6da8ea0c2c26062829d6aa141bd55849b7a
gildevson <gildevson@gmail.com>
2026-02-06 23:47:42 -0300

    Adicionando produtos

--- BackendApiWEB/Models/Produtos.cs
+++ BackendApiWEB/Models/Produtos.cs
@@ -4,11 +4,10 @@
         public string Nome { get; set; }
         public string ? Descricao { get; set; } // ? para dizer que é opcional pode ser Nula
         public decimal Preco { get; set; }
-
-
-
-
-
+        public string Estoque {get; set; }
+        public bool Ativo { get; set; } = true;
+        public DateTime DataCriacao { get; set; }
+        public DateTime? AtualizadoEm { get; set; }
 
     }
 }
