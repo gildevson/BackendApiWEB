0a0f490737efabde154dfa8c5d423e03cbeaec27
gildevson <gildevson@gmail.com>
2026-02-06 23:32:50 -0300

    Adicionando produtos

--- BackendApiWEB/Models/Produtos.cs
+++ BackendApiWEB/Models/Produtos.cs
@@ -1,10 +1,4 @@
-﻿namespace BackendApiWEB.Models
-{
-    public class Produtos
-    {
-        public string Token { get; set; }
-        public string NomeProduto { get; set; }
-        public DateTime DataCriacao { get; set; }
-
+﻿namespace BackendApiWEB.Models {
+    public class Produtos {
     }
 }
