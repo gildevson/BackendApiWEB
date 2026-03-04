de84da5380480ae4d203d31743a5a1410b84bf85
gilsonfonseca92@gmail.com <gilson.silva@finanblue.com.vc>
2026-03-04 13:12:13 -0300

    Adicionando dados de request classe usuarios

---
+++ BackendApiWEB/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿namespace BackendApiWEB.Models
+{
+    public class Cliente
+    {
+        Guid id { get; set; }
+        string nome { get; set; }
+        string cnpjcpf { get; set; }
+        string cep { get; set; }
+        string cidade { get; set; }
+        string endereco { get; set; }
+        string bairro { get; set; }
+        string Numero { get; set; }
+        string complemento { get; set; }
+        string Estado { get; set; } 
+        DateTime dataCadastro { get; set; }
+
+    }
+}
