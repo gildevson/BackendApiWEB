ad454e7c5584c369bd80c42bd8b505b0cb1cda6c
gilsonfonseca92@gmail.com <gilson.silva@finanblue.com.vc>
2026-03-06 13:42:52 -0300

    Adicioanndo mais ddos

--- BackendApiWEB/Models/Produtos.cs
+++ BackendApiWEB/Models/Produtos.cs
@@ -2,7 +2,7 @@
     public class Produtos {
         public Guid Id { get; set; }
         public string Nome { get; set; }
-        public string ? Descricao { get; set; } // ? para dizer que é opcional pode ser Nula
+        public string ? Descricao { get; set; } // ? para dizer que é opcional pode ser Nulas
         public decimal Preco { get; set; }
         public int Estoque {get; set; }
         public bool Ativo { get; set; } = true;
