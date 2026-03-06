d08732eace4aa146f71f420512062b72d4cf7d9e
gilsonfonseca92@gmail.com <gilson.silva@finanblue.com.vc>
2026-03-06 12:33:54 -0300

    Adicionando mais dados

--- BackendApiWEB/Service/Interfaces/IAuthService.cs
+++ BackendApiWEB/Service/Interfaces/IAuthService.cs
@@ -4,14 +4,10 @@ namespace BackendApiWEB.Service.Interfaces {
     public interface IAuthService {
         AuthResult Login(LoginRequest request);
         AuthResult Registrar(RegistrarRequest request);
-
-        // ✅ fica só este
         AuthResult Delete(Guid id, Guid idLogado);
-
         AuthResult SolicitarResetSenha(ResetSenhaSolicitarRequest dto);
         AuthResult ValidarToken(string token);
         AuthResult ResetarSenha(ResetSenhaRequest dto);
-
         AuthResult Update(UpdateUserRequest request);
     }
 }
