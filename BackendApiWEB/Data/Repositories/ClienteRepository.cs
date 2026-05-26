207c895974c5fa7f31871ffa16bfd80ce7284323
gilsonfonseca92@gmail.com <gilson.silva@finanblue.com.vc>
2026-05-26 13:39:38 -0300

    Adicioanndo mais dados

--- BackendApiWEB/Data/Repositories/ClienteRepository.cs
+++ BackendApiWEB/Data/Repositories/ClienteRepository.cs
@@ -22,12 +22,11 @@ namespace BackendApiWEB.Data.Repositories
             => _conn.Query<Cliente>
             (@"SELECT id, Nome, CnpjCpf, Email, Telefone, Endereco, Cidade, Estado, CEP, Ativo, CriadoEm, AtualizadoEm FROM dbo.Clientes");
 
-
-
+        
         public Guid Insert(Cliente c, IDbConnection conn, IDbTransaction? tran = null) {
             conn.Execute(@"INSERT INTO dbo.Clientes (id, nome, CnpjCpf, Email, Telefone, Endereco, Cidade, Estado, CEP, Ativo, CriadoEm, AtualizadoEm)
-            VALUES(@id, @Nome, @Cnpj, );",
-                new { c.Id, c.Nome, c.CnpjCpf, c.Email, c.Telefone, c.Endereco, c.Cidade, c.Estado, c.Cep, c.Ativo}, 
+            VALUES(@id, @Nome, @Cnpj, @Email, @Telefone, @Endereco, @Cidade, @Estado, @Cep, @Ativo, @CriadoEm, @Atualizado);",
+                new { c.Id, c.Nome, c.CnpjCpf, c.Email, c.Telefone, c.Endereco, c.Cidade, c.Estado, c.Cep, c.Ativo, c.CriadoEm, c.AtualizadoEm}, 
                 tran);
             return c.Id; 
             
